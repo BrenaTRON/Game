c6b070c3a21f8bd5e5179d7b73440dfa47796f5c
Tej Tummala <tej.tummala@gmail.com>
2019-05-06 14:54:04 -0700

    j

--- lol.java
+++ lol.java
@@ -4,11 +4,6 @@
  *
  * @author (your name)
  * @version (a version number or a date)
-<<<<<<< HEAD
- * // TEj TWas dawerer
-=======
- * i was 
->>>>>>> c42a1ee10dd500c3c26f810158dd1552d88c1122
  */
 public class lol
 {
