3269f273226b393d86d99c75b8de98bbef62dc2b
Brennan-Dai <brennandai@outlook.com>
2019-05-13 14:53:43 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,13 @@
-import javafx.application.Application;
-import javafx.scene.*;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.StackPaneBuilder;
-import javafx.stage.Stage;
+public class ImageButton extends Parent {
 
-public class ToggleButtonImage extends Application {
-  public static void main(String[] args) throws Exception { launch(args); }
-  @Override public void start(final Stage stage) throws Exception 
-  {
-    final ToggleButton toggle = new ToggleButton();
-    toggle.getStylesheets().add(this.getClass().getResource(
-      "imagetogglebutton.css"
-    ).toExternalForm());
-    toggle.setMinSize(148, 148); toggle.setMaxSize(148, 148);
-    stage.setScene(new Scene(
-      StackPaneBuilder.create()
-        .children(toggle)
-        .style("-fx-padding:10; -fx-background-color: cornsilk;")
-        .build()
-    ));
-    stage.show();
-  }
+    private static final Image NORMAL_IMAGE = null;
+    private static final Image PRESSED_IMAGE = null;
+
+    private final ImageView iv;
+
+    public ImageButton() {
+        this.iv = new ImageView(NORMAL_IMAGE);
+        this.getChildren().add(this.iv);
+        });
+    } 
 }
