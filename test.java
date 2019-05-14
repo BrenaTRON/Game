34d4048ecfba20e020561a7ec5487de0081cc9ef
Brennan-Dai <brennandai@outlook.com>
2019-05-14 14:52:34 -0700

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+import javafx.scene.control.*;
+import javafx.scene.Scene;
+import javafx.scene.image.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Tutorial extends Stage
+{
+    public static void main(String[] args)
+    {
+        Stage stage = new Stage();
+        stage.show();
+    }
+}
